15893ef5db7e61a798f2f3bf10aeaba786a9e93f
Ljubisa <ljubisa.rs@gmail.com>
2024-07-03 19:36:01 +0200

    wip(full): split logic to smaller functions and reduce calls to BlockByHash

--- pkg/full/full.go
+++ pkg/full/full.go
@@ -5,29 +5,31 @@ import (
 	"fmt"
 	"math/big"
 	"strconv"
+	"sync"
 
 	"github.com/gacevicljubisa/swaplist/pkg/transaction"
 	"github.com/go-playground/validator/v10"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/gacevicljubisa/swaplist/pkg/ethclient"
 )
 
 type Client struct {
 	validate        *validator.Validate
 	client          *ethclient.Client
+	lastBlock       *types.Block
 	blockRangeLimit uint32
+	mutex           sync.Mutex // To protect lastBlock
 }
 
 func NewClient(client *ethclient.Client, blockRangeLimit uint32) *Client {
-	c := &Client{
+	return &Client{
 		validate:        validator.New(),
 		client:          client,
 		blockRangeLimit: blockRangeLimit,
 	}
-
-	return c
 }
 
 type TransactionsRequest struct {
@@ -41,97 +43,146 @@ func (c *Client) GetTransactions(ctx context.Context, tr *TransactionsRequest) (
 	transactionChan := make(chan transaction.Transaction, 10)
 	errorChan := make(chan error)
 
-	var toBlock *big.Int
-	var fromBlock *big.Int
+	if err := c.validateRequest(tr); err != nil {
+		errorChan <- fmt.Errorf("error validating request: %w", err)
+		close(transactionChan)
+		close(errorChan)
+		return transactionChan, errorChan
+	}
 
+	var toBlock, fromBlock *big.Int
 	if tr.EndBlock != 0 {
 		toBlock = big.NewInt(int64(tr.EndBlock))
 	}
-
 	if tr.StartBlock == 0 {
-		fromBlock = &big.Int{}
-		fromBlock = fromBlock.Sub(toBlock, big.NewInt(5))
+		fromBlock = new(big.Int).Sub(toBlock, big.NewInt(5))
 	} else {
 		fromBlock = big.NewInt(int64(tr.StartBlock))
 	}
 
-	if err := c.validateRequest(tr); err != nil {
-		defer close(transactionChan)
-		defer close(errorChan)
-		errorChan <- fmt.Errorf("error validating request: %w", err)
-		return nil, nil
+	go c.processTransactions(ctx, tr.Address, fromBlock, toBlock, transactionChan, errorChan)
+	return transactionChan, errorChan
+}
+
+func (c *Client) processTransactions(ctx context.Context, address string, fromBlock, toBlock *big.Int, transactionChan chan transaction.Transaction, errorChan chan error) {
+	defer close(transactionChan)
+	defer close(errorChan)
+
+	contractAddress := common.HexToAddress(address)
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{contractAddress},
+		FromBlock: fromBlock,
+		ToBlock:   toBlock,
 	}
 
-	go func() {
-		defer close(transactionChan)
-		defer close(errorChan)
+	logsChan := make(chan types.Log, 10)
+	go c.fetchLogs(ctx, query, logsChan, errorChan)
 
-		contractAddress := common.HexToAddress(tr.Address)
-
-		query := ethereum.FilterQuery{
-			Addresses: []common.Address{contractAddress},
-			FromBlock: fromBlock,
-			ToBlock:   toBlock,
+	for vLog := range logsChan {
+		tx, isPending, err := c.client.TransactionByHash(ctx, vLog.TxHash)
+		if err != nil {
+			errorChan <- fmt.Errorf("failed to retrieve transaction: %w", err)
+			return
+		}
+		if isPending {
+			continue
 		}
 
-		fmt.Printf("querying logs for address %s, from block %d to block %d\n", tr.Address, query.FromBlock, query.ToBlock)
-
-		logs, err := c.client.FilterLogs(ctx, query)
+		block, err := c.getBlockByHash(ctx, vLog.BlockHash)
 		if err != nil {
-			errorChan <- fmt.Errorf("failed to retrieve logs: %w", err)
+			errorChan <- fmt.Errorf("failed to retrieve block: %w", err)
 			return
 		}
 
-		for _, vLog := range logs {
-			tx, isPending, err := c.client.TransactionByHash(ctx, vLog.TxHash)
-			if err != nil {
-				errorChan <- fmt.Errorf("failed to retrieve transaction: %w", err)
-				return
-			}
-
-			if isPending {
-				continue
-			}
-
-			block, err := c.client.BlockByHash(ctx, vLog.BlockHash)
-			if err != nil {
-				errorChan <- fmt.Errorf("failed to retrieve block: %w", err)
-				return
-			}
-
-			var index uint
-
-			for idx, tr := range block.Transactions() {
-				if tr.Hash() == tx.Hash() {
-					index = uint(idx)
-					break
-				}
-			}
-
-			sender, err := c.client.TransactionSender(ctx, tx, vLog.BlockHash, index)
-			if err != nil {
-				errorChan <- fmt.Errorf("failed to retrieve sender: %w", err)
-				return
-			}
-
-			transactionChan <- transaction.Transaction{
-				From:      sender.Hex(),
-				TimeStamp: strconv.FormatUint(block.Time(), 10),
-			}
+		index, err := c.findTransactionIndex(block, tx)
+		if err != nil {
+			errorChan <- fmt.Errorf("failed to find transaction index: %w", err)
+			return
 		}
-	}()
 
-	return transactionChan, errorChan
+		sender, err := c.client.TransactionSender(ctx, tx, vLog.BlockHash, index)
+		if err != nil {
+			errorChan <- fmt.Errorf("failed to retrieve sender: %w", err)
+			return
+		}
+
+		transactionChan <- transaction.Transaction{
+			From:      sender.Hex(),
+			TimeStamp: strconv.FormatUint(block.Time(), 10),
+		}
+	}
 }
 
 func (c *Client) validateRequest(tr *TransactionsRequest) error {
 	if err := c.validate.Struct(tr); err != nil {
 		return err
 	}
-
 	if tr.StartBlock > tr.EndBlock {
 		return fmt.Errorf("start block should be less than or equal to end block")
 	}
-
 	return nil
+}
+
+func (c *Client) fetchLogs(ctx context.Context, query ethereum.FilterQuery, logsChan chan<- types.Log, errorChan chan error) {
+	defer close(logsChan)
+
+	maxBlocks := uint64(c.blockRangeLimit)
+	startBlock := query.FromBlock.Uint64()
+	endBlock := query.ToBlock.Uint64()
+
+	for start := startBlock; start <= endBlock; start += maxBlocks {
+		end := start + maxBlocks - 1
+		if end > endBlock {
+			end = endBlock
+		}
+
+		chunkQuery := ethereum.FilterQuery{
+			FromBlock: new(big.Int).SetUint64(start),
+			ToBlock:   new(big.Int).SetUint64(end),
+			Addresses: query.Addresses,
+			Topics:    query.Topics,
+		}
+
+		fmt.Printf("querying logs from block %d to block %d\n", chunkQuery.FromBlock.Uint64(), chunkQuery.ToBlock.Uint64())
+
+		logs, err := c.client.FilterLogs(ctx, chunkQuery)
+		if err != nil {
+			errorChan <- fmt.Errorf("failed to retrieve logs: %w", err)
+			return
+		}
+
+		for _, log := range logs {
+			select {
+			case logsChan <- log:
+			case <-ctx.Done():
+				errorChan <- ctx.Err()
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) getBlockByHash(ctx context.Context, blockHash common.Hash) (*types.Block, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if c.lastBlock != nil && c.lastBlock.Hash() == blockHash {
+		return c.lastBlock, nil
+	}
+
+	block, err := c.client.BlockByHash(ctx, blockHash)
+	if err != nil {
+		return nil, err
+	}
+	c.lastBlock = block
+	return block, nil
+}
+
+func (c *Client) findTransactionIndex(block *types.Block, tx *types.Transaction) (uint, error) {
+	for idx, bTx := range block.Transactions() {
+		if bTx.Hash() == tx.Hash() {
+			return uint(idx), nil
+		}
+	}
+	return 0, fmt.Errorf("transaction not found in block")
 }
